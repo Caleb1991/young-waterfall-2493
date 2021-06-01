f556a47402df44aee72c15a34e4491f6205715c5
Caleb Wittman <calebjwittman1991@gmail.com>
2021-06-01 10:07:06 -0600

    Complete user story 4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,5 +3,9 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
 
     @actors = Actor.joins(:actors_movies).where('movie_id = ?', @movie.id)
+
+    if params[:name] != nil
+      @search_results = Actor.where('name = ?', params[:name])
+    end
   end
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -25,20 +25,35 @@ RSpec.describe 'Movie Show Page' do
     visit "/movies/#{@movie_3.id}"
   end
 
-#     As a user,
-# When I visit a movie's show page.
-# I see the movie's title, creation year, and genre,
-# and I see all of the actors in the movie
-
   it 'Shows the movies attributes' do
     expect(page).to have_content(@movie_3.title)
     expect(page).to have_content(@movie_3.creation_year)
     expect(page).to have_content(@movie_3.genre)
   end
 
-  it 'Shows all the actors for the given movie' do
+  it 'Shows all the actors for the given movie and doesnt show actors not associated with the given movie' do
     expect(page).to have_content(@actor_2.name)
     expect(page).to have_content(@actor_3.name)
     expect(page).to_not have_content(@actor_1.name)
+  end
+
+#   As a user,
+# When I visit a movie show page,
+# And I see a form to add an actor to this movie
+# When I fill in the form with the name of an actor that exists in the database
+# And I click submit
+# Then I am redirected back to that movie's show page
+# And I see the actor's name is now listed
+
+  it 'has a form to add an actor' do
+    expect(page).to have_content('Name')
+  end
+
+  it 'returns to movie show page and shows results for searched actor' do
+    fill_in :name, with: 'Matt Smith'
+    click_on 'Search for Actor'
+
+    expect(current_path).to eq("/movies/#{@movie_3.id}")
+    expect(page).to have_content('Matt Smith')
   end
 end
